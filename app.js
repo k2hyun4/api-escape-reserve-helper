59a9b195d931b800792ff4871a933212dab6f3e5
khkim <khkim@gaia3d.com>
2022-06-02 14:07:19 +0900

    init express server

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+	res.send('Hello k2h!');
+});
+
+app.get('/test', (req, res) => {
+	const {a, b} = req.query;
+	const numberA = Number(a);
+	const numberB = Number(b);
+
+	res.json({
+		a: numberA,
+		b: numberB,
+		sum: numberA + numberB
+	});
+});
+
+app.listen(port, () => console.log('Server Start in ', port));
